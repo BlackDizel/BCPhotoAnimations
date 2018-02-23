d3a3003580224129d8053acad222e7e8f280a48a
blackdizel <blackdizel@gmail.com>
2018-02-23 19:58:34 +0300

    add navigator

---
+++ app/src/main/java/org/byters/bcphotoanimations/view/INavigator.java
@@ -0,0 +1,18 @@
+package org.byters.bcphotoanimations.view;
+
+import android.support.v4.app.FragmentManager;
+
+public interface INavigator {
+
+    void setData(FragmentManager fragmentManager, int flContent);
+
+    void navigateProjectsList();
+
+    void navigateProjectEdit();
+
+    void navigateProjectCreate();
+
+    boolean closeProjectEdit();
+
+    boolean onBackPressed();
+}
---
+++ app/src/main/java/org/byters/bcphotoanimations/view/Navigator.java
@@ -0,0 +1,70 @@
+package org.byters.bcphotoanimations.view;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import org.byters.bcphotoanimations.view.ui.fragment.FragmentProjectEdit;
+import org.byters.bcphotoanimations.view.ui.fragment.FragmentProjects;
+
+import java.lang.ref.WeakReference;
+
+public class Navigator implements INavigator {
+
+    public static final String TAG_PROJECT_EDIT = "TAG_PROJECT_EDIT";
+    private WeakReference<FragmentManager> refFragmentManager;
+    private int rootViewRes;
+
+    @Override
+    public void setData(FragmentManager fragmentManager, int flContent) {
+        this.refFragmentManager = new WeakReference<>(fragmentManager);
+        this.rootViewRes = flContent;
+    }
+
+    @Override
+    public void navigateProjectsList() {
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(rootViewRes, new FragmentProjects())
+                .commit();
+    }
+
+    @Override
+    public void navigateProjectEdit() {
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+
+        Fragment fragment = refFragmentManager.get().findFragmentByTag(TAG_PROJECT_EDIT);
+        if (fragment != null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .add(rootViewRes, new FragmentProjectEdit(), TAG_PROJECT_EDIT)
+                .commit();
+    }
+
+    @Override
+    public void navigateProjectCreate() {
+        navigateProjectEdit();
+    }
+
+    @Override
+    public boolean closeProjectEdit() {
+        if (refFragmentManager == null || refFragmentManager.get() == null) return false;
+
+        Fragment fragment = refFragmentManager.get().findFragmentByTag(TAG_PROJECT_EDIT);
+        if (fragment == null) return false;
+        refFragmentManager.get()
+                .beginTransaction()
+                .remove(fragment)
+                .commit();
+
+        return true;
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        if (closeProjectEdit()) return true;
+
+        return false;
+    }
+}
