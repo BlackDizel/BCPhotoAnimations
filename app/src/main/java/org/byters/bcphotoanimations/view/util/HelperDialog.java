19fd18327b1adfd1657783676eeb2b9f732fe338
Alexandr <blakdizel@gmail.com>
2019-03-31 16:26:29 +0300

    dialog helper

---
+++ app/src/main/java/org/byters/bcphotoanimations/view/util/HelperDialog.java
@@ -0,0 +1,32 @@
+package org.byters.bcphotoanimations.view.util;
+
+import org.byters.bcphotoanimations.view.ui.activity.ActivityBase;
+import org.byters.bcphotoanimations.view.ui.dialog.DialogExportJCodec;
+import org.byters.bcphotoanimations.view.ui.dialog.IDialog;
+
+import java.lang.ref.WeakReference;
+
+public class HelperDialog implements IHelperDialog {
+    private WeakReference<ActivityBase> refContext;
+    private WeakReference<IDialog> refDialog;
+
+    @Override
+    public void set(ActivityBase activityBase) {
+        this.refContext = new WeakReference<>(activityBase);
+    }
+
+    @Override
+    public void showDialogExportMJPEG() {
+        if (refContext == null || refContext.get() == null) return;
+        showDialog(new DialogExportJCodec(refContext.get()));
+    }
+
+    private void showDialog(IDialog dialog) {
+        if (refDialog != null && refDialog.get() != null)
+            refDialog.get().cancel();
+
+        refDialog = new WeakReference<>(dialog);
+        refDialog.get().show();
+
+    }
+}
---
+++ app/src/main/java/org/byters/bcphotoanimations/view/util/IHelperDialog.java
@@ -0,0 +1,9 @@
+package org.byters.bcphotoanimations.view.util;
+
+import org.byters.bcphotoanimations.view.ui.activity.ActivityBase;
+
+public interface IHelperDialog {
+    void set(ActivityBase activityBase);
+
+    void showDialogExportMJPEG();
+}
