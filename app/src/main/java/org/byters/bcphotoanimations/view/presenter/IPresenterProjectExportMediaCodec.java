a10e386913076a163037923d49fb5e5f59a98679
Alexandr <blakdizel@gmail.com>
2019-03-31 18:52:50 +0300

    mediacodec export dialog

---
+++ app/src/main/java/org/byters/bcphotoanimations/view/presenter/IPresenterProjectExportMediaCodec.java
@@ -0,0 +1,5 @@
+package org.byters.bcphotoanimations.view.presenter;
+
+public interface IPresenterProjectExportMediaCodec {
+    void onClickExport(String fps, String w, String h);
+}
---
+++ app/src/main/java/org/byters/bcphotoanimations/view/ui/dialog/DialogExportMediaCodec.java
@@ -0,0 +1,59 @@
+package org.byters.bcphotoanimations.view.ui.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import org.byters.bcphotoanimations.ApplicationStopMotion;
+import org.byters.bcphotoanimations.R;
+import org.byters.bcphotoanimations.view.presenter.IPresenterProjectExportJCodec;
+import org.byters.bcphotoanimations.view.presenter.IPresenterProjectExportMediaCodec;
+
+import javax.inject.Inject;
+
+public class DialogExportMediaCodec implements IDialog, View.OnClickListener {
+    private final AlertDialog dialog;
+
+    @Inject
+    IPresenterProjectExportMediaCodec presenter;
+    private TextView tvFps, tvWidth, tvHeight;
+
+    public DialogExportMediaCodec(Context context) {
+        ApplicationStopMotion.getComponent().inject(this);
+        dialog = new AlertDialog.Builder(context)
+                .create();
+
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_export_mediacodec, null);
+
+        initViews(view);
+        dialog.setView(view);
+    }
+
+    private void initViews(View view) {
+        tvFps = view.findViewById(R.id.etFPS);
+        tvWidth = view.findViewById(R.id.etWidth);
+        tvHeight = view.findViewById(R.id.etHeight);
+
+        view.findViewById(R.id.tvExport).setOnClickListener(this);
+    }
+
+    @Override
+    public void cancel() {
+        dialog.cancel();
+    }
+
+    @Override
+    public void show() {
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tvExport) {
+            cancel();
+            presenter.onClickExport(tvFps.getText().toString(), tvWidth.getText().toString(), tvHeight.getText().toString());
+        }
+    }
+}
