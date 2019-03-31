4f88e89a8585d6f14682cd81ebd1f3595b585655
Alexandr <blakdizel@gmail.com>
2019-03-31 16:25:44 +0300

    implement export jcodec in service

--- app/src/main/java/org/byters/bcphotoanimations/view/ui/service/ServiceProjectExport.java
+++ app/src/main/java/org/byters/bcphotoanimations/view/ui/service/ServiceProjectExport.java
@@ -7,6 +7,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -24,6 +25,7 @@ import org.byters.bcphotoanimations.controller.data.memorycache.ICacheStorage;
 import org.byters.bcphotoanimations.view.ui.activity.ActivityMain;
 import org.byters.bcphotoanimations.view.ui.utils.AsyncTaskProjectExport;
 import org.byters.bcphotoanimations.view.ui.utils.AsyncTaskProjectExportListener;
+import org.byters.bcphotoanimations.view.util.AsyncTaskExportMP4_JCodec;
 
 import javax.inject.Inject;
 
@@ -33,6 +35,13 @@ public class ServiceProjectExport extends Service {
     private static final String SERVICE_NOTIFICATION_CHANNEL_ID = "service_notification";
 
     private static final String EXTRA_PROJECT_ID = "project_id";
+    private static final String EXTRA_EXPORT_TYPE = "export_type";
+    private static final int EXPORT_TYPE_UNKNOWN = 0;
+    private static final int EXPORT_TYPE_IMAGES = 1;
+    private static final int EXPORT_TYPE_MJPEG = 2;
+    private static final String EXTRA_EXPORT_FPS = "video_fps";
+    private static final String EXTRA_EXPORT_FRAME_WIDTH = "video_width";
+    private static final String EXTRA_EXPORT_FRAME_HEIGHT = "video_height";
 
     @Inject
     ICacheProjects cacheProjects;
@@ -43,14 +52,26 @@ public class ServiceProjectExport extends Service {
     @Inject
     ICacheExportAttempts cacheExportAttempts;
 
-    private AsyncTaskProjectExport worker;
+    private AsyncTask<Void, Integer, Boolean> worker;
     private NotificationCompat.Builder notificationBuilder;
     private ListenerExportTask listenerExportTask;
 
-    public static void start(Context context, String projectId) {
+    public static void startExportImages(Context context, String projectId) {
         Intent intent = new Intent(context, ServiceProjectExport.class);
 
         intent.putExtra(EXTRA_PROJECT_ID, projectId);
+        intent.putExtra(EXTRA_EXPORT_TYPE, EXPORT_TYPE_IMAGES);
+        context.startService(intent);
+    }
+
+    public static void startExportMJPEG(Context context, String projectId, int w, int h, int fps) {
+        Intent intent = new Intent(context, ServiceProjectExport.class);
+
+        intent.putExtra(EXTRA_PROJECT_ID, projectId);
+        intent.putExtra(EXTRA_EXPORT_TYPE, EXPORT_TYPE_MJPEG);
+        intent.putExtra(EXTRA_EXPORT_FPS, fps);
+        intent.putExtra(EXTRA_EXPORT_FRAME_WIDTH, w);
+        intent.putExtra(EXTRA_EXPORT_FRAME_HEIGHT, h);
         context.startService(intent);
     }
 
@@ -78,7 +99,14 @@ public class ServiceProjectExport extends Service {
 
         startForeground(SERVICE_NOTIFICATION_ID, notificationBuilder.build());
 
-        export(projectId);
+        int type = intent.getIntExtra(EXTRA_EXPORT_TYPE, EXPORT_TYPE_UNKNOWN);
+        if (type == EXPORT_TYPE_IMAGES)
+            exportImages(projectId);
+        if (type == EXPORT_TYPE_MJPEG)
+            exportVideo(projectId,
+                    intent.getIntExtra(EXTRA_EXPORT_FPS, 0),
+                    intent.getIntExtra(EXTRA_EXPORT_FRAME_WIDTH, 0),
+                    intent.getIntExtra(EXTRA_EXPORT_FRAME_HEIGHT, 0));
 
         return START_STICKY;
     }
@@ -111,6 +139,7 @@ public class ServiceProjectExport extends Service {
                 NotificationManager.IMPORTANCE_HIGH);
 
         notificationChannel.setDescription(getString(R.string.notification_project_export_channel_description));
+        notificationChannel.setSound(null, null);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if (notificationManager == null) return;
@@ -118,11 +147,20 @@ public class ServiceProjectExport extends Service {
         notificationManager.createNotificationChannel(notificationChannel);
     }
 
-    private void export(String projectId) {
+    private void exportImages(String projectId) {
         if (worker != null) worker.cancel(true);
         worker = new AsyncTaskProjectExport(projectId,
                 cacheProjects,
                 cacheStorage,
+                listenerExportTask);
+        worker.execute();
+    }
+
+
+    private void exportVideo(String projectId, int fps, int w, int h) {
+        if (worker != null) worker.cancel(true);
+
+        worker = new AsyncTaskExportMP4_JCodec(projectId, fps, w, h,
                 listenerExportTask);
         worker.execute();
     }
@@ -138,13 +176,24 @@ public class ServiceProjectExport extends Service {
         @Override
         public void onUpdate(String projectId, Integer[] values) {
             if (values == null
-                    || values.length != 2) return;
+                    || values.length < 2) return;
 
             notificationBuilder.setProgress(values[0], values[1], false);
             String title = cacheProjects.getItemTitleById(projectId);
-            if (!TextUtils.isEmpty(projectId))
-                notificationBuilder.setContentText(String.format(getString(R.string.notification_project_export_title_progress_format), title, values[1] + 1));
+            String message = TextUtils.isEmpty(projectId)
+                    ? getString(R.string.export_frame_message_error)
+                    : values.length == 2
+                    ? String.format(getString(R.string.notification_project_export_title_progress_format), title, values[1] + 1)
+                    : values.length == 3
+                    ? String.format(getString(R.string.notification_project_export_title_progress_format_with_state), title, values[1] + 1, getState(values[2]))
+                    : getString(R.string.export_frame_message_error);
+
+            notificationBuilder.setContentText(message);
             updateNotification();
+        }
+
+        private String getState(int value) {
+            return getString(value == 1 ? R.string.export_frame_state_success : R.string.export_frame_state_error);
         }
 
         @Override
